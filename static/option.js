b010e215b606966e90265ef200a8096f3798d7f8
georgeitea <mdarad@hotmail.com>
2025-08-28 19:40:11 +0300

    Delete static directory

--- static/option.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("server").onclick = function(){
-   window.location.href = "/server";
-}
-
-
-document.getElementById("controller").onclick = function(){
-   window.location.href = "/controller";
-}
