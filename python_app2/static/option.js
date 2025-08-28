e322e36f9ff90e609f3278d7d365e37198d87d75
georgeitea <mdarad@hotmail.com>
2025-08-28 19:42:53 +0300

    Add files via upload

---
+++ python_app2/static/option.js
@@ -0,0 +1,8 @@
+document.getElementById("server").onclick = function(){
+   window.location.href = "/server";
+}
+
+
+document.getElementById("controller").onclick = function(){
+   window.location.href = "/controller";
+}
